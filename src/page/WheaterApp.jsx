9b41d0ecb212b035d7f567d8cfa6e7f23a2ecc8a
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-31 11:21:27 -0300

    :wrench: feat: create main page

---
+++ src/page/WheaterApp.jsx
@@ -0,0 +1,44 @@
+
+import '../styles/WeatherApp.scss'
+
+
+function WeatherApp() {
+
+
+
+
+    return (
+        <div className='background'>
+            <header>
+                <h1>WeatherApp</h1>
+            </header>
+
+            <main>
+
+                <section className='weatherInfo'>
+                    <div>
+                        <p>Olá</p>
+
+                    </div>
+
+                </section>
+
+
+                <section>
+
+
+                </section>
+
+
+            </main>
+
+
+
+        </div>
+    )
+
+}
+
+
+
+export default WeatherApp
