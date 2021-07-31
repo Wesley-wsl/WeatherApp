0c1ae10159df9b5d9e48216b77028d27d80e9537
Wesley Westelley <wesleywestelley@gmail.com>
2021-07-31 11:24:04 -0300

    :wrench: feat: create main route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './style/App.scss';
+import './styles/App.scss';
+import WeatherApp from './page/WheaterApp'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                    <Route exact path='/' component={WeatherApp}></Route>
+            </BrowserRouter>
+
+        </div>
+    );
 }
 
 export default App;
